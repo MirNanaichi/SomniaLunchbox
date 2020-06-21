4be3993907bc71969ac3443d2753819a26a93ee8
MirNanaichi <33403430+MirNanaichi@users.noreply.github.com>
2020-06-22 00:29:44 +0200

    Update 22.06.2020
    update

---
+++ Somnia/Lunchbox/Assets/Script/colorchange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorchange : MonoBehaviour
+{
+    //Reference to Scripte Renderer component
+    private Renderer rend;
+
+    //Color value that we can set in Inspector
+    //It's write by Default
+    [SerializeField]
+    private Color colorToTurnTo = Color.white;
+
+    //Use this for initialization
+    private void Start()
+    {
+        //Assign Renderer component to rend variable
+        rend = GetComponent<Renderer>();
+
+        //change sprite color to selected color
+        rend.material.color = colorToTurnTo;
+    }
+}
